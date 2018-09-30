102319fc12c24e322667c6a0a3949c8f863c3f83
Ilya Zimonin <ilyazimonin@gmail.com>
2018-09-30 11:52:48 +0300

    Initial commit

---
+++ mobilelab/mobilelab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobilelab
+//
+//  Created by Ilya Zimonin on 30/09/2018.
+//  Copyright © 2018 Ilya Zimonin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobilelab/mobilelab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mobilelab
+//
+//  Created by Ilya Zimonin on 30/09/2018.
+//  Copyright © 2018 Ilya Zimonin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
